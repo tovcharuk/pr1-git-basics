c85e71275dda51e5854caaa7365e510cca3f4dbe
unknown <t.ovcharyk@e-u.edu.ua>
2026-02-26 17:36:01 +0200

    docs: add run instructions and example

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-print("Hello World")
-
 def celsius_to_fahrenheit(temp):
     return temp * 9 / 5 + 32
 
+if __name__ == '__main__':
+    temp_celsius = 10
+    print(f"{temp_celsius} degrees Celsius is {celsius_to_fahrenheit(temp_celsius)} degrees Fahrenheit")
